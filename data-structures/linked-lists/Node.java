2650608340457196c689804fe93e58648fb6900b
Md Abul Kalam Azad <azadmd.tech@gmail.com>
2026-03-12 22:29:04 -0400

    Update Node class to support both singly and doubly linked lists

--- data-structures/linked-lists/Node.java
+++ data-structures/linked-lists/Node.java
@@ -1,25 +1,42 @@
 /**
- * Represents a node in a singly linked list.
- * Each node stores an integer value and a reference to the next node.
+ * Represents a node used in linked list data structures.
+ *
+ * This node supports both:
+ * - Singly Linked Lists (uses the next reference)
+ * - Doubly Linked Lists (uses both next and prev references)
+ *
+ * Each node stores an integer value and references to neighboring nodes.
+ *
+ * Fields:
+ * data : the value stored in the node
+ * next : reference to the next node in the list
+ * prev : reference to the previous node (used in doubly linked lists)
  *
  * @author Md Abul Kalam Azad
  */
 public class Node {
 	
 	int data;
-	Node next;
-	
-	//Default Constructor
-	public Node() {
-		this.data = 0;
-		this.next = null;
-	}
-	
-	//constructor with data
-	public Node(int data) {
-		this.data = data;
-		this.next = null;
-	}
-	
+    Node next;
+    Node prev;
 
+    /**
+     * Default constructor.
+     * Initializes a node with value 0 and null references.
+     */
+    public Node() {
+        this(0);
+    }
+
+    /**
+     * Creates a node with the specified data value.
+     *
+     * @param data value to store in the node
+     */
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+	
 }
